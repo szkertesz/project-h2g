c729bab9f287c48fb97d8bc837f6144c6e693c04
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-05-31 13:51:24 +0200

    isPalindrome task

---
+++ 03_js/is-palindrome.js
@@ -0,0 +1,20 @@
+const isPalindrome = (text) => {
+    // solution #1
+    // const clearText = text.replace(/\s+/g, '');
+    // if (!clearText) {
+    //     return false;
+    // }
+    // const reversedText = clearText.split('').reverse().join('');
+    // return reversedText === clearText ? true : false;
+
+    // solution #1 refactored
+    const clearText = text.replace(/\s+/g, '');
+    return clearText && clearText === clearText.split('').reverse().join('') ? true : false;
+}
+
+console.log(isPalindrome('')); // false
+console.log(isPalindrome('     ')); // false
+console.log(isPalindrome('anna')); // true
+console.log(isPalindrome('anna    ')); // true
+console.log(isPalindrome('indul a gorog aludni')); // true
+console.log(isPalindrome('alma')); // false
