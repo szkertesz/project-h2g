2cbfe4be17fa632f1986339567c09aaf0156e38e
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-06-01 16:41:10 +0200

    getUUIDBlocks - array.from solution

---
+++ 03_js/get-uuid-blocks.js
@@ -0,0 +1,6 @@
+const getUUIDBlocks = uuid => {
+    const clearUuidArr = Array.from(uuid.replace(/\-/g, ''));
+    return Array.from({length: clearUuidArr.length / 2}, (v, i) => [clearUuidArr[2 * i], clearUuidArr[2 * i + 1]].join(''))
+}
+
+console.log(getUUIDBlocks('f782f011-636a-4d8b-9f63-dc34a4503c01')); // ['f7', '82', 'f0', '11', '63', '6a', '4d', '8b', '9f', '63', 'dc', '34',' a4', '50', '3c', '01']
