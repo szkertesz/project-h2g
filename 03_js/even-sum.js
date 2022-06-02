392daa5e40e50f95909bfb74da2bbcc5d66f4a13
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-06-02 11:01:33 +0200

    even-sum #1

---
+++ 03_js/even-sum.js
@@ -0,0 +1,9 @@
+const evenSum = (array) => {
+    const flatArr = array.flat(Infinity)
+    return flatArr.reduce((sum, current) => {
+        return current % 2 === 0 ? sum + current : sum
+    }, 0)
+}
+
+console.log(evenSum([1, [2, 3], [[4], 5]])); // 6
+console.log(evenSum([1, [1, 1], [[1], 1]])); // 0
