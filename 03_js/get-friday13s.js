833077aaacb165fec338bcbf31b47a96c3b563ac
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-06-02 17:22:17 +0200

    squareAndSum + getFriday13s

---
+++ 03_js/get-friday13s.js
@@ -0,0 +1,21 @@
+const getFriday13s = (startYear, endYear) => {
+    const rangeStart = new Date(startYear, 0, 1);
+    const rangeEnd = endYear ? new Date(endYear, 12, 31) : new Date(startYear, 12, 31);
+
+    const getDaysArray = (start, end) => {
+        const arr = []
+        for (const dt = new Date(start); dt <= new Date(end); dt.setDate(dt.getDate() + 1)) {
+            arr.push(new Date(dt));
+        }
+        return arr;
+    };
+
+    return getDaysArray(rangeStart, rangeEnd)
+        .filter((date) => {
+            return date.getDay() === 5 && date.getDate() === 13
+        })
+        .map(friday => new Intl.DateTimeFormat('hu').format(friday))
+}
+
+console.log(getFriday13s(1999, 2000)); // ['1999.08.13.', '2000.10.13']
+console.log(getFriday13s(2000)); // ['2000.10.13']
---
+++ 03_js/square-and-sum.js
@@ -0,0 +1,18 @@
+"use strict";
+7
+const formattedElement = (number) => [...number.toString()].reduce((total, current) => total + Number(current), 0)
+const squareAndSum = (array) => {
+    const formattedArray = [];
+    array.forEach(element => {
+        if (element < 0) {
+            element = Math.pow(element, 2)
+        }
+        let tempElement = formattedElement(element)
+        while (tempElement.toString().length > 1) {
+            tempElement = formattedElement(tempElement)
+        }
+        formattedArray.push(tempElement)
+    });
+    return formattedArray
+}
+console.log(squareAndSum([-1, 1, 12, -4, -5, 999])); // [1, 1, 3, 7, 7, 9]
