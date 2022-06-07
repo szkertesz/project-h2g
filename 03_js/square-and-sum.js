1237745a000e18e4bfb1ae895b5d9129a27bc5a0
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-06-07 16:26:47 +0200

    Refactor sumDigits

--- 03_js/square-and-sum.js
+++ 03_js/square-and-sum.js
@@ -1,18 +1,33 @@
 "use strict";
+// Solution #1
+// const sumDigits = (number) => [...number.toString()].reduce((total, current) => total + Number(current), 0)
 
-const formattedElement = (number) => [...number.toString()].reduce((total, current) => total + Number(current), 0)
+// const squareAndSum = (array) => {
+//     const formattedArray = [];
+//     array.forEach(element => {
+//         if (element < 0) {
+//             element = Math.pow(element, 2)
+//         }
+//         let tempElement = sumDigits(element)
+//         while (tempElement.toString().length > 1) {
+//             tempElement = sumDigits(tempElement)
+//         }
+//         formattedArray.push(tempElement)
+//     });
+//     return formattedArray
+// }
+
+// Refactoring
+const sumDigits = number => Array.from(number.toString()).reduce((total, current) => total + Number(current), 0)
+
 const squareAndSum = (array) => {
-    const formattedArray = [];
-    array.forEach(element => {
+    return array.map(element => {
         if (element < 0) {
             element = Math.pow(element, 2)
         }
-        let tempElement = formattedElement(element)
-        while (tempElement.toString().length > 1) {
-            tempElement = formattedElement(tempElement)
-        }
-        formattedArray.push(tempElement)
+        let summedElement = sumDigits(element)
+        return summedElement.toString().length > 1 ? sumDigits(summedElement) : summedElement
     });
-    return formattedArray
 }
+
 console.log(squareAndSum([-1, 1, 12, -4, -5, 999])); // [1, 1, 3, 7, 7, 9]
