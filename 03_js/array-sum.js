56920256fa6e0ba183f7175c498e7d2fda289299
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-06-07 10:40:43 +0200

    arraySum #1

---
+++ 03_js/array-sum.js
@@ -0,0 +1,10 @@
+const arraySum = (array) => {
+    return array.reduce((sum, current) => {
+        return Array.isArray(current) ?
+            sum + arraySum(current) :
+            typeof current === 'number' && isFinite(current) ?
+                sum + current : sum
+    }, 0);
+}
+console.log(arraySum([1, [2, [[[3]]]], [4, 5], 100])); // 115
+console.log(arraySum([1, [2, [3], [4, '5', null, undefined, [NaN, Infinity], [true, false], { id: '1' }, 5]]])); // 15
