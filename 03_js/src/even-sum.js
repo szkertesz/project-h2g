6b8f62720a8894d066080bee7bb87ac00c23fc06
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-06-13 21:33:19 +0200

    Add 2nd solution to evenSum

--- 03_js/src/even-sum.js
+++ 03_js/src/even-sum.js
@@ -11,5 +11,22 @@ export const evenSum = (array) => {
     }, 0)
 }
 
+export const evenSum2 = (array) => {
+    if (!array) {
+        return -1
+    }
+    if (!Array.isArray(array)) {
+        return -1
+    }
+    return array.reduce((sum, current) => {
+        return Array.isArray(current) ?
+            sum + evenSum2(current) :
+            current % 2 === 0 ?
+                sum + current : sum
+    }, 0)
+}
+
 // console.log(evenSum([1, [2, 3], [[4], 5]])); // 6
 // console.log(evenSum([1, [1, 1], [[1], 1]])); // 0
+// console.log(evenSum2([1, [2, 3], [[4], 5]])); // 6
+// console.log(evenSum2([1, [1, 1], [[1], 1]])); // 0
