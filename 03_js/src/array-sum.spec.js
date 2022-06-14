1e153746616b6d8ecab0582b417365773bbf3841
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-06-14 08:48:22 +0200

    Add more descriptive test titles to arraySum tests

--- 03_js/src/array-sum.spec.js
+++ 03_js/src/array-sum.spec.js
@@ -1,35 +1,35 @@
 import { arraySum } from "./array-sum";
 
 describe('arraySum', () => {
-    it ('should return 0', () => {
+    it ('should return 0 when input is an empty array', () => {
         const array = []
         const actual = arraySum(array)
         const expected = 0
 
         expect(actual).toEqual(expected)
     })
-    it ('should return 0', () => {
+    it ('should return 0 if input is undefined', () => {
         const array = undefined
         const actual = arraySum(array)
         const expected = 0
 
         expect(actual).toEqual(expected)
     })
-    it ('should return 0', () => {
+    it ('should return 0 if input is Infinity', () => {
         const array = Infinity
         const actual = arraySum(array)
         const expected = 0
 
         expect(actual).toEqual(expected)
     })
-    it ('should return 15', () => {
+    it('should return 15 if [1, 2, 3, 4, 5] is given', () => {
         const array = [1, 2, 3, 4, 5]
         const actual = arraySum(array)
         const expected = 15
 
         expect(actual).toEqual(expected)
     })
-    it ('should return 15', () => {
+    it('should return 15 if input is [1, 2, 3, 4, \'5\', null, undefined, NaN, Infinity, true, false, { id: \'1\' }, 5]', () => {
         const array = [1, 2, 3, 4, '5', null, undefined, NaN, Infinity, true, false, { id: '1' }, 5]
         const actual = arraySum(array)
         const expected = 15
