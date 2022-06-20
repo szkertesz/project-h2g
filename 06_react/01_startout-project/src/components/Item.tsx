3c7bf511b64c0ea7697fb57ebc652f69c8a55573
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-06-20 17:43:42 +0200

    Rewrite Item FC, convert it to class component

--- 06_react/01_startout-project/src/components/Item.tsx
+++ 06_react/01_startout-project/src/components/Item.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Description } from './Description.interface';
 
-function List(props: { description: Description }) {
-    const renderHTML = (rawHTML: string) => {
-        return React.createElement('article', {
-            dangerouslySetInnerHTML: { __html: rawHTML },
-        });
+// function List(props: { description: Description }) {
+//     const renderHTML = (rawHTML: string) => {
+//         return React.createElement('article', {
+//             dangerouslySetInnerHTML: { __html: rawHTML },
+//         });
+//     };
+//     return <li>{renderHTML(props.description.translations.hu)}</li>;
+// };
+
+
+// export default Item
+
+class Item extends React.Component<{description: Description}> {
+    render() {
+        const renderHTML = (rawHTML: string) => {
+            return React.createElement('article', {
+                dangerouslySetInnerHTML: { __html: rawHTML },
+            });
+        }
+        return (
+            <li>{renderHTML(this.props.description.translations.hu)}</li>
+        )
     };
-    return <li>{renderHTML(props.description.translations.hu)}</li>;
-};
+}
 
-
-export default List
+export default Item;
