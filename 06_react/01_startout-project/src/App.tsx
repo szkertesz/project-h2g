164c3f7cfe360d57a9460e35035f0fe4d0c9dc10
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-06-21 15:02:53 +0200

    try... catch-ify async fetch

--- 06_react/01_startout-project/src/App.tsx
+++ 06_react/01_startout-project/src/App.tsx
@@ -12,17 +12,26 @@ function App() {
     const now = Date.now();
 
     const getData = async () => {
-        const response = await fetch(
-            `https://futar.bkk.hu/api/query/v1/ws/otp/api/where/alert-search.json?&version=3&appVersion=3&start=${now}`
-        );
-        const alertsData = await response.json();
-        const alerts = alertsData.data.references.alerts;
-        // convert the `alerts`property of AlertsData object to an array
-        const alertsArray = Object.keys(alerts).map(
-            (key) => alerts[key]
-        ) as Alert[];
-        setAlertsLoaded(alertsArray);
-        setIsLoading(false);
+        try {
+            const response = await fetch(
+                `https://futar.bkk.hu/api/query/v1/ws/otp/api/where/alert-search.json?&version=3&appVersion=3&start=${now}`
+            );
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            const alertsData = await response.json();
+            const alerts = alertsData.data.references.alerts;
+            // convert the `alerts`property of AlertsData object to an array
+            const alertsArray = Object.keys(alerts).map(
+                (key) => alerts[key]
+            ) as Alert[];
+            setAlertsLoaded(alertsArray);
+            setIsLoading(false);
+        } catch (error) {
+          if (error instanceof Error) {
+            console.warn(String(error.message))
+          }
+        }
     };
 
     useEffect(() => {
