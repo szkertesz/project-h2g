f159e887c3e829fd709f25b028fe5a04d2ef1bb9
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-06-24 13:30:14 +0200

    Build radio interface for GenreFilter

--- 06_react/02_react-netflix-roulette/src/components/GenreFilter.tsx
+++ 06_react/02_react-netflix-roulette/src/components/GenreFilter.tsx
@@ -1,25 +1,70 @@
+import React, { useEffect } from "react";
+
 function GenreFilter() {
+    const [value, setValue] = React.useState('all')
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value)
+   }
     return (
-        <div>
-            <ul>
-                <li>
-                    <button>all</button>
-                </li>
-                <li>
-                    <button>documentary</button>
-                </li>
-                <li>
-                    <button>comedy</button>
-                </li>
-                <li>
-                    <button>horror</button>
-                </li>
-                <li>
-                    <button>crime</button>
-                </li>
-            </ul>
-        </div>
+        <fieldset>
+            <legend></legend>
+            <div>
+                <input
+                    type='radio'
+                    name='genre'
+                    id='all'
+                    value='all'
+                    checked={value === 'all'}
+                    onChange={handleChange}
+                />
+                <label htmlFor='all'>All</label>
+            </div>
+            <div>
+                <input
+                    type='radio'
+                    name='genre'
+                    id='documentary'
+                    value='documentary'
+                    checked={value === 'documentary'}
+                    onChange={handleChange}
+                />
+                <label htmlFor='documentary'>documentary</label>
+            </div>
+            <div>
+                <input
+                    type='radio'
+                    name='genre'
+                    id='comedy'
+                    value='comedy'
+                    checked={value === 'comedy'}
+                    onChange={handleChange}
+                />
+                <label htmlFor='comedy'>comedy</label>
+            </div>
+            <div>
+                <input
+                    type='radio'
+                    name='genre'
+                    id='horror'
+                    value='horror'
+                    checked={value === 'horror'}
+                    onChange={handleChange}
+                />
+                <label htmlFor='horror'>horror</label>
+            </div>
+            <div>
+                <input
+                    type='radio'
+                    name='genre'
+                    id='crime'
+                    value='crime'
+                    checked={value === 'crime'}
+                    onChange={handleChange}
+                />
+                <label htmlFor='crime'>crime</label>
+            </div>
+        </fieldset>
     );
 }
 
-export default GenreFilter
+export default GenreFilter;
