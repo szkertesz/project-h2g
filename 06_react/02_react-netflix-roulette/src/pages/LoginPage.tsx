c01a7797b34874f4df583dda7ef861c613730606
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-06-30 17:45:35 +0200

    Build login page

--- 06_react/02_react-netflix-roulette/src/pages/LoginPage.tsx
+++ 06_react/02_react-netflix-roulette/src/pages/LoginPage.tsx
@@ -1,7 +1,29 @@
+import Button from "../ui/Button";
+import classes from './LoginPage.module.scss'
+
 function LoginPage() {
-    return <section>
-        <h1>Login page</h1>
-    </section>
+    return (
+        <div className={classes.login}>
+            <section className={classes['login__content']}>
+                <h1 className={classes['login__title']}>Login page</h1>
+                <form action='' className={classes['login__form']}>
+                    <div className={classes['login__group']}>
+                        <label htmlFor='userId'>user ID*</label>
+                        <input type='email' id='userId' />
+                    </div>
+                    <div className={classes['login__group']}>
+                        <label htmlFor='password'>Password*</label>
+                        <input type='password' id='password' />
+                    </div>
+                    <div className={classes['login__action']}>
+                        <Button isGhost={true}>reset</Button>
+                        <Button>Log in</Button>
+                    </div>
+                    <p>*Mandatory</p>
+                </form>
+            </section>
+        </div>
+    );
 }
 
-export default LoginPage
+export default LoginPage;
