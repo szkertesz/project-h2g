fc8fdc7e3cc65a5ace56de9c33557e4869dab832
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-07-04 12:33:32 +0200

    Get moviedata w/ RTK Query

--- 06_react/02_react-netflix-roulette/src/components/MovieList.tsx
+++ 06_react/02_react-netflix-roulette/src/components/MovieList.tsx
@@ -1,55 +1,68 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import MovieItem from './MovieItem';
 import { Movie } from '../interfaces/Movie';
-import classes from './MovieList.module.scss'
+import classes from './MovieList.module.scss';
+
+// import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { useGetDataQuery } from '../features/api/apiSlice';
 
 function MovieList() {
-    const [isLoading, setIsLoading] = useState(false);
-    const [movieData, setMovieData] = useState<Movie[]>([]);
+    // const [isLoading, setIsLoading] = useState(false);
+    // const [movieData, setMovieData] = useState<Movie[]>([]);
 
-    const getData = async () => {
-        try {
-            const response = await fetch('movies.json', {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Accept: 'application/json',
-                },
-            });
-            if (!response.ok) {
-                throw new Error(response.statusText);
-            }
-            const data = await response.json();
-            const moviesData = data.movies
-            setMovieData(moviesData);
-            setIsLoading(false)
-        } catch (error) {
-            if (error instanceof Error) {
-                console.warn(String(error.message));
-            }
-        }
-    };
+    // const getData = async () => {
+    //     try {
+    //         const response = await fetch('movies.json', {
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //                 Accept: 'application/json',
+    //             },
+    //         });
+    //         if (!response.ok) {
+    //             throw new Error(response.statusText);
+    //         }
+    //         const data = await response.json();
+    //         const moviesData = data.movies;
+    //         setMovieData(moviesData);
+    //         setIsLoading(false);
+    //     } catch (error) {
+    //         if (error instanceof Error) {
+    //             console.warn(String(error.message));
+    //         }
+    //     }
+    // };
 
-    useEffect(() => {
-        getData();
-    }, []);
+    // useEffect(() => {
+    //     getData();
+    // }, []);
 
-    if(isLoading) {
-        return(
-            <section>
-                <h2 className='visually-hidden'>Loading State</h2>
-                <p>Loading...</p>
-            </section>
-        )
-    }
+    // if(isLoading) {
+    //     return(
+    //         <section>
+    //             <h2 className='visually-hidden'>Loading State</h2>
+    //             <p>Loading...</p>
+    //         </section>
+    //     )
+    // }
+    const { data, isFetching } = useGetDataQuery();
+    const movieData = data?.movies;
+
+    if (isFetching) return <section>Loading...</section>;
+    if (!data) return <section><p>Can't get any data :-/</p></section>;
+
     return (
         <section>
             <h2 className='visually-hidden'>Results</h2>
-            <p className={classes.info}><span>{movieData.length}</span> movies found</p>
+            {/* <p className={classes.info}>
+                <span>{movieData.length}</span> movies found
+            </p> */}
+            <p className={classes.info}>
+                <span>{movieData?.length}</span> movies found
+            </p>
             <ul className={classes.results}>
-                {movieData.map((movie: Movie) => {
+                {movieData?.map((movie: Movie) => {
                     return (
                         <li key={movie.id}>
-                            {/* <MovieItem {...movie} /> */}
                             <MovieItem movieInfo={movie} />
                         </li>
                     );
