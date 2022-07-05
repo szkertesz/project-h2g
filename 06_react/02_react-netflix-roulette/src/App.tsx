561431a5e092eabf56702b7b09eff99471fc0469
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-07-05 11:24:37 +0200

    Reorganize routes & page related components

--- 06_react/02_react-netflix-roulette/src/App.tsx
+++ 06_react/02_react-netflix-roulette/src/App.tsx
@@ -1,25 +1,39 @@
-import { Route, Routes } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
-import Layout from './ui/Layout'
-import MoviesPage from './pages/MoviesPage';
-import MovieDetailsPage from './pages/MovieDetailsPage';
-import ModalPage from './pages/ModalPage';
-import AddMovieFormPage from './pages/AddMovieFormPage';
+import { Route, Routes } from 'react-router-dom';
+import { Movie } from './interfaces/Movie';
+import { useGetDataQuery } from './features/api/apiSlice';
 import './App.css';
+import HomeAndMovieDetailsPageLayout from './pages/HomeAndMovieDetailsPageLayout';
+import HomePageHeader from './pages/home-page/HomePageHeader';
+import MovieDetailsPageHeader from './pages/movie-details-page/MovieDetailsPageHeader';
+import LoginPage from './pages/LoginPage';
+
+/* Temporary pages for component demos */
+import AddMovieFormPage from './pages/AddMovieFormPage';
+import ModalPage from './pages/ModalPage';
 
 function App() {
+    const { data } = useGetDataQuery();
+    const movieData = data?.movies as Movie[];
+    // const movieIds = movieData?.map((movie: Movie, index: number) => movie.id)
+    // console.log(movieIds)
+    console.warn(movieData);
     return (
-        <Routes>
-            <Route path='/' element={<Layout />}>
-                <Route index element={<MoviesPage />}></Route>
-                <Route path='movies' element={<MoviesPage />}></Route>
-                <Route path='movie' element={<MovieDetailsPage />}></Route>
-            </Route>
-            <Route path='/login' element={<LoginPage />}></Route>
-            {/* Temporary pages for component demos */}
-            <Route path='modal' element={<ModalPage />}></Route>
-            <Route path='add-movie' element={<AddMovieFormPage />}></Route>
-        </Routes>
+        <>
+            <Routes>
+                <Route path='*' element={<HomeAndMovieDetailsPageLayout />}>
+                    <Route index element={<HomePageHeader />}></Route>
+                    <Route path='movies' element={<HomePageHeader />}></Route>
+                    <Route
+                        path='movie/*'
+                        element={<MovieDetailsPageHeader />}
+                    ></Route>
+                </Route>
+                <Route path='/login' element={<LoginPage />}></Route>
+                {/* Temporary pages for component demos */}
+                <Route path='modal' element={<ModalPage />}></Route>
+                <Route path='add-movie' element={<AddMovieFormPage />}></Route>
+            </Routes>
+        </>
     );
 }
 
